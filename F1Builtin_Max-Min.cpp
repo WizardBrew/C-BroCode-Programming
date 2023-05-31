504f9ab7f3d795e7d736092a0da28bb6fdde396f
WizardBrew <wizardbrew84@gmail.com>
2023-06-01 02:38:50 +0530

    F1Builtin_Min-Max

---
+++ F1Builtin_Max-Min.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+        double x = 10;
+        double y = 20;
+        double z;
+        float test =3.145;
+
+        // z= max (x, y);                         // Shows Maximum 
+        // z= min (x, y);                           //Shows        Minimum
+        // z = pow(2, 3);                       //Shows  Power/ Exponental
+        // z = sqrt(9);                               //Shows   Square root
+        // z = abs(-3);                               //Shows   absolute value without +/- 
+        // z = round(3.84);                     //Shows    Rounds near by number
+        // test = round(test);                //Shows    Rounds near by number
+        // test = ceil(test);                   //Shows  Rounds further number
+        // test = floor(test);                  //Shows rounds below value
+
+        cout<<z<< endl;
+        cout<<test<< endl;
+
+        return 0;
+}
