99425cde5d461d1b742126ef6d77893610736964
WizardBrew <wizardbrew84@gmail.com>
2023-06-04 00:11:51 +0530

    A3User_Input_Inter

--- A3User_Input.cpp
+++ A3User_Input.cpp
@@ -8,19 +8,19 @@ using std::endl;
 
 
 int main( ){
-        int age, workexp;                                        // Declearation of integer
+        int age, workexp;                                        // Declearation of integer can declear same type in one.
         string name, company, city, yes;             // Declearation of strings, so i can Direct initialze
         
         
-        cout<<"Enter your name  : \n";          // Asking the Question- Dispaly OP
-        getline(cin>>std::ws, name);            // majorly Cin is used if there is no space.
-        cout<<"Your Age : \n" ;                         // Age 
-        cin>>age;                                                       // Enter the Age in OP
+        cout<<"Enter your name  : \n";                          // Asking the Question- Dispaly OP
+        getline(cin>>std::ws, name);                              // majorly Cin is used if there is no space.
+        cout<<"Your Age : \n" ;                                          // Age 
+        cin>>age;                                                                 // Enter the Age in OP
         cout<<"Enter your Company Details  :\n";        
-        getline(cin>>std::ws, company );        // getline allows to take input of may words. using -ws 
-        cout<<"Work Exp  :\n"; cin>>workexp;  // other way but I do not recomend it.
+        getline(cin>>std::ws, company );                         // getline allows to take input of may words. using -ws 
+        cout<<"Work Exp  :\n"; cin>>workexp;            // other way but I do not recomend it.
         cout<<"Enter the City name  : ";
-        getline(cin>>std::ws, city);                    // WS is imp bcz it clears Buffer of the line whiteSpace
+        getline(cin>>std::ws, city);                              // WS is imp bcz it clears Buffer of the line whiteSpace
 
 cout<<"-------- == Your Details ==------------------\n";
         cout<<"Name : "<<name<< endl;
