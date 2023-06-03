dcb982e5da36909173eda053861710a0a04830c5
WizardBrew <wizardbrew84@gmail.com>
2023-06-03 23:35:06 +0530

    H2IfElse_Inter

---
+++ H2IfElse_Basic2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        int age;
+cout<<"Enter the age : ";
+cin>>age;
+
+        if (age>=69 && age<=100){
+                cout<<"You Need to rest. ";
+        }
+        else if(age>=18 && age <=68){
+                cout<<"Your are Qualified 18+ Club";
+        }
+        else if(age>=1 && age<=18){
+                cout<<"You are not Qualified Grow up";
+        }
+
+        else{
+                cout<<"Your Input is not valid";
+        }
+
+
+
+return 0;
+}
