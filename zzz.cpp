00fb49685a30cba42314d14440e4820fd58f62a7
WizardBrew <wizardbrew84@gmail.com>
2023-06-06 01:24:33 +0530

    Practise_Rough

--- zzz.cpp
+++ zzz.cpp
@@ -6,14 +6,26 @@ using std::cin;
 using std::endl;
 
 int main( ){
-
-string name = "WizardBrew";
-        cout<<name.find("B");           // find (the dat want to find) eg : 'B' or "B"  
-                                                                //can find "Brew" but shows first char index
+        string name =  "Wizard";
+        string name2 = "Brew";
         
-       
+        name.begin();
+        cout<<name;
 return 0;
 }
+
+// void swapstring(){
+//          string name =  "Wizard";
+//         string name2 = "Brew";
+//         int a= 5, b =6;
+//         string x;
+//         //     name.swap(name2);    // Method 1
+//               std::swap(name, name2); // Method 2
+//               cout<<name<<"\n";
+//               cout<<name2<<'\n'  ; 
+// }
+
+
 void precisions(){
          // double a = 6.352356663353535;
         // std::cout.precision(8);
