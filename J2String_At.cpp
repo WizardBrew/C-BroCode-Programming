21da4d4af91cc0fbccfeadde344a0b546ec51345
WizardBrew <wizardbrew84@gmail.com>
2023-06-06 00:51:55 +0530

    J2String_At

---
+++ J2String_At.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name = "WizardBrew";
+               cout<< name.at(6);                     //at (place or Index to find)
+
+return 0;
+}
