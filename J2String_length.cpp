e41d08b021a0efaa18903dfd5167bffc5d9c8a4c
WizardBrew <wizardbrew84@gmail.com>
2023-06-06 00:30:43 +0530

    J2String_length

---
+++ J2String_length.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name = "WizardBrew";
+        cout<<name.length();            //length (Empty)        as it shows the length 
+
+return 0;
+}
