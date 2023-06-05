52122af0b40aec22f6768b593fb19c83f2dca7a5
WizardBrew <wizardbrew84@gmail.com>
2023-06-06 00:30:32 +0530

    J2String_Instert

---
+++ J2String_Instert.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name = "WizarBrew";
+        name.insert(6, "-");            // instert ( which_Place,   entered data needed )
+
+return 0;
+}
