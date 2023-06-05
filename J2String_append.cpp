955f50509436014576ce594576a807d1e15b3b7b
WizardBrew <wizardbrew84@gmail.com>
2023-06-06 00:29:19 +0530

    J2String_Append

---
+++ J2String_append.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name = "WizardBrew";
+
+        name.append("@outlook.com");    // append(what ever you need at the end of var)
+        cout<<name;
+        
+        
+        
+return 0;
+}
