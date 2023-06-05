b5e4a578287ac47b8eea103899c8c2ba2d86ec7f
WizardBrew <wizardbrew84@gmail.com>
2023-06-06 00:29:39 +0530

    J2String_Find

---
+++ J2String_Find.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name = "WizardBrew";
+        cout<<name.find("B");           // find (the dat want to find) eg : 'B' or "B"  
+                                                                //can find "Brew" but shows first char index
+        
+
+return 0;
+}
