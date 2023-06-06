1917592a69069fde6f3445685a1f7f235d58406e
WizardBrew <wizardbrew84@gmail.com>
2023-06-06 23:30:44 +0530

    J2String_Erase

---
+++ J2String_Erase.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name = "WizardBrew";
+
+        cout<<name.erase(0, 6 );  // Delete the char from 0 to 6.
+
+return 0;
+}
