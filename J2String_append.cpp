f67f9e91ccaee2f8d309686b29495be91239119c
WizardBrew <wizardbrew84@gmail.com>
2023-06-07 00:01:36 +0530

    J2String_append

--- J2String_append.cpp
+++ J2String_append.cpp
@@ -6,7 +6,10 @@ using std::cin;
 using std::endl;
 
 int main( ){
-        string name = "WizardBrew";
+        string name ;    // = "WizardBrew";
+
+        cout<<"Enter the name: ";
+        getline(cin, name);
 
         name.append("@outlook.com");    // append(what ever you need at the end of var)
         cout<<name;
