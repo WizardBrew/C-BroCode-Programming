8ea90bcecb0490e6a3f96b768f3005d9eda7de1c
WizardBrew <wizardbrew84@gmail.com>
2023-06-07 00:01:47 +0530

    J2String_At

--- J2String_At.cpp
+++ J2String_At.cpp
@@ -6,7 +6,11 @@ using std::cin;
 using std::endl;
 
 int main( ){
-        string name = "WizardBrew";
+        string name ;   //  = "WizardBrew";
+
+        cout<<"Enter the name: ";
+        getline(cin, name);
+
                cout<< name.at(6);                     //at (place or Index to find)
 
 return 0;
