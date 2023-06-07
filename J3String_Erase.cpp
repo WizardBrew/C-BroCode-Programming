29d02ec87eef1e641cec55813b2eff0b3f693193
WizardBrew <wizardbrew84@gmail.com>
2023-06-07 23:45:27 +0530

    J3String_Erase

--- J3String_Erase.cpp
+++ J3String_Erase.cpp
@@ -10,7 +10,8 @@ int main( ){
         cout<<"Enter the name: ";
         getline(cin, name);
 
-        cout<<name.erase(0, 6 );  // Delete the char from 0 to 6.
+        // cout<<name.erase(0, 6 );  // Delete the char from 0 to 6.
+        cout<<name.erase(5, 8 );  // Delete the char from 0 to 6.
 
 return 0;
 }
