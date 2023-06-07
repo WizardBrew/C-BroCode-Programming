d197c49df11833f103ff4f6686d94830c8ced37a
WizardBrew <wizardbrew84@gmail.com>
2023-06-08 00:29:36 +0530

    K1WhileLoop_Basis2

---
+++ K1WhileLoop_Basis2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        int a;
+        cout<<"Enter the Number you want to print : ";
+        cin>>a;
+
+        while(a<=10){
+                cout<<a<<"\n";
+                a++; }
+
+return 0;
+}
