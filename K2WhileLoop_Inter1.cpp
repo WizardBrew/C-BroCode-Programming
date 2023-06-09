dfedafd8f5fd74182785cddf9267a18020782b3b
WizardBrew <wizardbrew84@gmail.com>
2023-06-09 23:44:35 +0530

    K2WhileLoop_Inter1

---
+++ K2WhileLoop_Inter1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name;
+
+        while(1==1){
+                cout<<"Wizard \t Brew ";
+                cout<<"Wizard \b Brew ";
+        }
+
+return 0;
+}
