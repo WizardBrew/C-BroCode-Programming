82520594c1807d372cbc8c6eb60553f339c87ddf
WizardBrew <wizardbrew84@gmail.com>
2023-06-09 23:41:36 +0530

    K2WhileLoop_Inter

---
+++ K2WhileLoop_Inter.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        string name;
+        string name1;
+        
+        cout<<"Enter Here :"; cin>>name1;
+                if (name1 == "Loop"){
+                        while(name.empty()){
+                        cout<<"Enter the Name \"You cannot keep it Empty\" :\n";
+                        getline(cin,name);
+                }
+                cout<<"Hey Hi :"<<name;
+        }
+        else{
+                cout<<"Ok got it you need something else :";
+        }        
+
+
+return 0;
+}
