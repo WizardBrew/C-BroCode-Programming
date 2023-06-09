dd69b6a579ee9a0e57c993d35c2aa786ab7ee775
WizardBrew <wizardbrew84@gmail.com>
2023-06-10 00:02:55 +0530

    L1DoWhile_Basic

--- L1DoWhile_Basic.cpp
+++ L1DoWhile_Basic.cpp
@@ -7,13 +7,14 @@ using std::endl;
 
 int main( ){
         string name;
-        cout<<"Enter Name: ";
-        cin>>name;
 
-        while(name.length()>4){
+        // cout<<"Enter Name: ";
+        // cin>>name;
+
+        do{
                 cout<<"Enter name : ";
                 cin>>name;
-        }
+        } while(name.length()>4);
         cout<<"Hey you did it by typing : "<<name;
 
 return 0;
