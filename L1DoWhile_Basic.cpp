9c62532e8cc88b2c45e941304961ed41569d3192
WizardBrew <wizardbrew84@gmail.com>
2023-06-11 23:54:29 +0530

    L1DoWhile_Basic

--- L1DoWhile_Basic.cpp
+++ L1DoWhile_Basic.cpp
@@ -14,7 +14,7 @@ int main( ){
         do{
                 cout<<"Enter name : ";
                 cin>>name;
-        } while(name.length()>4);
+        } while(name.length()>4);                       // if the Length of input name is more then 4 doesnt accept
         cout<<"Hey you did it by typing : "<<name;
 
 return 0;
