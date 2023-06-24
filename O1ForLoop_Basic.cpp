16ad927db40dae6da439f07dc9b0f89cab9a3342
WizardBrew <wizardbrew84@gmail.com>
2023-06-24 22:39:15 +0530

    O1ForLoop_Basic.exe

---
+++ O1ForLoop_Basic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        for(int i =1; i<=5; i++){
+                for(int j = 1; j<=5; j++){
+                        cout<< i <<' ';
+                }
+                cout<<'\n';
+        }
+
+return 0;
+}
