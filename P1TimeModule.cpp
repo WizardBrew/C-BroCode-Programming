85557d7573cc194f6621f8aead39137f033d0302
WizardBrew <wizardbrew84@gmail.com>
2023-06-24 23:11:45 +0530

    P1TimeModule

--- P1TimeModule.cpp
+++ P1TimeModule.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<ctime>
+// #include<ctime>         // Not required can work in this file basic file untill time is used
 
 using std::cout;
 using std::string;
@@ -7,8 +7,8 @@ using std::cin;
 using std::endl;
 
 int main( ){
-        std::srand(static_cast<unsigned>(std::time(nullptr)));
-        // srand(time(NULL));              // pseduo random Numbers = Not Truely Random (But Close)
+        
+        srand(time(NULL));              // pseduo random Numbers = Not Truely Random (But Close)
                 //  int num =rand() ;                       // Usually Genrates long code so can we take reminder of 6, 20 or 100
 
         int num =(rand() %6) + 1 ;
