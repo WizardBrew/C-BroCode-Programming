90e93659838a99bdee0a10a55ab453803a3c6a6f
WizardBrew <wizardbrew84@gmail.com>
2023-06-24 22:40:38 +0530

    M2FoorLoop_Matrix.cpp

--- M2FoorLoop_Matrix.cpp
+++ M2FoorLoop_Matrix.cpp
@@ -4,7 +4,7 @@ using std::cout;
 using std::string;
 using std::cin;
 using std::endl;
-
+ 
 int main( ){
         for(char i = 65; i <=75; i++){          // Loop will run from Char i =65 thats A till next 10
                 for(int j = 1; j<=5; j++ ){             // j will run alomg with i in matrix formate
