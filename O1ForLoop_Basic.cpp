226d11355c4a29df1f225a4f23a0d57e8686628d
WizardBrew <wizardbrew84@gmail.com>
2023-06-24 22:43:07 +0530

    O1ForLoop_Nested_Basic

--- O1ForLoop_Basic.cpp
+++ O1ForLoop_Basic.cpp
@@ -5,7 +5,7 @@ using std::string;
 using std::cin;
 using std::endl;
 
-int main( ){
+int main( ){ 
         for(int i =1; i<=5; i++){
                 for(int j = 1; j<=5; j++){
                         cout<< i <<' ';
