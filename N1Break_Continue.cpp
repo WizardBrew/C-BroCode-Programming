f4bf5a5c92c13d0e49d735592645923253493bd6
WizardBrew <wizardbrew84@gmail.com>
2023-06-24 22:37:51 +0530

    Create N1Break_Continue.cpp

---
+++ N1Break_Continue.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main( ){
+        
+        for(int i = 1; i <=20; i++){
+                if(i==13){
+                        // cout<<"Reached Unlucky \n"; 
+                        // break;
+                        continue;
+                }
+                cout<<i<<'\n';
+        }
+
+return 0;
+}
