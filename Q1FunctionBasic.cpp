36ec488ed9bf655d4eea402b81b1b455449e03ea
WizardBrew <wizardbrew84@gmail.com>
2023-07-13 01:12:02 +0530

    Q1FunctionBasic

---
+++ Q1FunctionBasic.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+void happyBirthday();           // Define the Function if yoou are using it after the int 
+                                                      //  so it can be searched later  when checked in main int
+
+int main( ){
+
+happyBirthday();                      // Calling function in the main.
+
+return 0;
+}
+
+
+void happyBirthday(){           // Creating Happybirthday Function
+        cout<<"Happy Birthday \n";
+        cout<<"Happy Birthday \n";
+
+}
