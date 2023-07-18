6ed8df089e5d95b6d73b1efc93909c456451d5ba
WizardBrew <wizardbrew84@gmail.com>
2023-07-19 00:09:26 +0530

    Projects\project2Banking.cpp

---
+++ Projects/project2Banking.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<iomanip>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+void showBalance(double balance);
+double deposit();
+double withdrawal(double balance);
+
+int main( ){
+       double balance = 786.786 ;
+        int choice = 0;
+
+        do{      
+                cout<<"The Banking Application \n";
+                cout<<"======================= \n";
+                cout<<"  1. | Show Balance \n";
+                cout<<"  2. | Deposit Amount  \n";
+                cout<<"  3. | Withdraw  \n";
+                cout<<"  4. | EXIT \n";
+                
+                cin>>choice;
+                cin.clear();
+                fflush(stdin);
+
+                switch(choice){
+                        case 1 : showBalance( balance);
+                                        break;
+                        case 2 : balance = balance + deposit();
+
+                                        showBalance(balance);
+                                        break;
+                        case 3 : balance = balance - withdrawal(balance);
+
+                                        showBalance(balance);
+                                        break;
+                        case 4 :  cout<<"Thanks for visiting \n";
+                                break;
+                        default: cout<<"Invalid Choice \n";
+                }
+        } while (choice != 4);
+
+        return 0;
+}
+
+void showBalance(double balance){
+        cout<<"Your Balance is - $ "<< std::setprecision(2)<<std::fixed<<balance<<'\n';
+        
+}
+double deposit(){
+        double amount = 0;
+        cout<<"Enter the amount you want to Deposit --> ";
+        cin>>amount;
+        
+        if(amount>0){
+                 return amount;
+        }
+        else{
+                cout<<"You are not entering the Valid Amount ";
+                return 0;
+        }
+}
+double withdrawal(double balance){
+        double amount = 0;
+        cout<<"Enter the amount you want to Withdraw --> ";
+        cin>>amount;
+
+        if(amount>balance){
+                cout<<"Your amout is insufficient to withdraw - ";
+                return 0;
+        }
+        else if (amount<0){
+                cout<<"Invalid Amout ";
+                return 0;
+        }        
+        else{
+                return amount;  
+        }
+
+}
