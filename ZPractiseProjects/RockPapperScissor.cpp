136d5312db12878dbea74a24642e0a497339259a
WizardBrew <wizardbrew84@gmail.com>
2023-07-25 02:49:20 +0530

    RockPapperScissor

--- ZPractiseProjects/RockPapperScissor.cpp
+++ ZPractiseProjects/RockPapperScissor.cpp
@@ -6,6 +6,55 @@ using std::string;
 using std::cin;
 using std::endl;
 
+void lowerFunc(string str) ;      
+void playerChoice(char choice);
+char computerChoice();
+char showChoice(char choice);
+void ShowWinner();              
+
+int main( ){
+
+        string str ;
+        char choice;
+        char choice1;
+        // // cout<<"Enter The Text\n";
+        // // getline(cin>>std::ws, str);
+        // lowerFunc(str);
+        
+        choice1 = showChoice(choice);
+
+        playerChoice(choice1);
+        
+
+        
+
+        return 0;
+}
+void playerChoice(char choice){
+        // char choice;
+        switch(choice){
+                case 'r': cout<<"You Choose  -= Rock =- ";
+                        break;
+                case 's': cout<<"You Choose  -= Sicssor =- ";
+                        break;
+                case 'p': cout<<"You Choose  -= Paper =- ";
+                        break;
+        }
+
+}
+
+char showChoice(char choice){
+        do{
+        cout<<"Enter Choice\n";
+        cin>>choice;
+        choice = tolower(choice);
+        cout<<" = Rock - Paper - Sicssor";
+        cout<<"\n ------======--------\n";
+        cout<<"Choose Among this \n = R= | = P= | = S = \n";
+        }while(choice != 'r' && choice!= 's' && choice!= 'p');
+        return choice;
+}
+
 void lowerFunc(string str){                     // str will take input
       int  length = str.length();                       // take length of string as dynamic
         for (int i = 0 ;i < length; i++ ){       // Alwasy specify i=0 dont leave blank or it will not consider start if in function
@@ -13,19 +62,4 @@ void lowerFunc(string str){                     // str will take input
                 str[i] = tolower(convert);
         }
         cout<< str;  
-}
-
-int main( ){
-
-        string str ;
-        cout<<"Enter The Text\n";
-        getline(cin>>std::ws, str);
-
-        lowerFunc(str);
-
-        cout<<"\n--------------------\n";
-        cout<<str;
-        
-
-return 0;
 }
