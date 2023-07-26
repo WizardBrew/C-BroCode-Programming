fb6538a21bd335c3c8af6a59a25a2cfad7599c9e
WizardBrew <wizardbrew84@gmail.com>
2023-07-27 01:25:33 +0530

    S1ArrayBasic

---
+++ S1ArrayBasic.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+void albs(){
+        string album[3];                // Always Define size if you want to set elements later
+        album[0] = "Doorie";
+        album[1] = "Jannat";
+        album[2] = "Khuda";
+        
+        cout<<album[0]  <<"\n";
+        cout<<album[1] <<"\n";
+        cout<<album[2] <<"\n";
+}
+ void musics(){
+        string music[] = {"Aatif Aslam", "KK", "Rahat Fathe", "Darshan Rawal", "Sonu Nigam"};
+
+        cout<<music[0] << '\n';        
+        cout<<music[1] << '\n';        
+        cout<<music[2] << '\n';  
+ }
+
+int main( ){ 
+
+        albs();
+        cout<<'\n';
+        musics();
+
+              
+
+
+return 0;
+}
