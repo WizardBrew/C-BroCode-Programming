03f619729380e79b4b215862a341f1be1c260525
WizardBrew <wizardbrew84@gmail.com>
2023-07-29 00:38:16 +0530

    ArrayBasicSearch

---
+++ ZPractiseProjects/ArrayBasicSearch.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+
+
+int main( ){
+
+        double myArray[] = {1,2,3,4,5,6,7,8,9,10};
+        int size = sizeof(myArray)/sizeof(myArray[0]);
+        int index;
+        int myNum;
+        int mysrch;
+        cout<<'\n';
+        cout<<"There are total -= " <<size<<" =- Elements in Array \n -- ";
+
+        cin >> myNum;
+
+        for(int i = 0 ; i< size ; i ++){
+                if (myArray[i]== myNum){
+                        cout<<"\n Found your num -= "<<myNum<<" =- Element at -=> "<<i;
+                        break;
+                }cout<<"Checking -- "<<i<<'\n';
+        }       return 0;
+
+return 0;
+        }
