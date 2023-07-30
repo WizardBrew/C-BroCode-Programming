bcdff8bc2659cb006acef84b62a794832772cf04
WizardBrew <wizardbrew84@gmail.com>
2023-07-30 15:01:02 +0530

    S3ArrayIntermediate

--- S3ArrayIntermediate.cpp
+++ S3ArrayIntermediate.cpp
@@ -8,24 +8,24 @@ using std::endl;
 
 double getTotal(double prices[], int size);
 
-int main( ){
+        int main( ){
 
-        double prices[] = { 49.99 , 15.05 , 75, 9.99};
-        double size = sizeof(prices)/sizeof(prices[0]);
-        double total = getTotal(prices, size);
-        cout<<" $ "<< total;
+                double prices[] = { 49.99 , 15.05 , 75, 9.99};
+                double size = sizeof(prices)/sizeof(prices[0]);
+                double total = getTotal(prices, size);
+                        cout<<" $ "<< total;
 
-return 0;
+        return 0;
 }
 
-double getTotal(double prices[], int size){
-        double sum = 0;
-        for(int i =0; i<size; i ++){
-                sum = sum + prices[i];
-                
-        }
+        double getTotal(double prices[], int size){
+                double sum = 0;
+                        for(int i =0; i<size; i ++){
+                                sum = sum + prices[i];
+                        
+                }
 
-        return sum;
+                return sum;
 }
 
 
