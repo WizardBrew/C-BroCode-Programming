e276c83f176f3b51188f378835545974b311c087
WizardBrew <wizardbrew84@gmail.com>
2023-08-20 01:12:59 +0530

    K2WhileLoop_Inter

--- K2WhileLoop_Inter.cpp
+++ K2WhileLoop_Inter.cpp
@@ -9,13 +9,13 @@ int main( ){
         string name;
         string name1;
         
-        cout<<"Enter Here :"; cin>>name1;
-                if (name1 == "Loop"){
-                        while(name.empty()){
+        cout<<"Enter Here :"; cin>>name1;               //takes input
+                if (name1 == "Loop"){                                 // checks condition 
+                        while(name.empty()){                            // checks if the Name inp is empty
                         cout<<"Enter the Name \"You cannot keep it Empty\" :\n";
-                        getline(cin,name);
-                }
-                cout<<"Hey Hi :"<<name;
+                        getline(cin,name);              // if its empty then again asks for inp
+                }               
+                cout<<"Hey Hi :"<<name;         // Takes input
         }
         else{
                 cout<<"Ok got it you need something else :";
