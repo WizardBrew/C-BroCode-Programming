0d66345afd515bba5e9048156984ac9957762aa4
WizardBrew <wizardbrew84@gmail.com>
2023-08-20 01:14:11 +0530

    K2WhileLoop_Inter2

--- K2WhileLoop_Inter2.cpp
+++ K2WhileLoop_Inter2.cpp
@@ -6,15 +6,15 @@ using std::cin;
 using std::endl;
 
 int main( ){
-        string name;
-        cout<<"Enter Name: ";
-        cin>>name;
+        string name;                            // Var
+        cout<<"Enter Name: ";           // Display
+        cin>>name;                              // Takes input
 
-        while(name.length()>4){
-                cout<<"Enter name : ";
-                cin>>name;
+        while(name.length()>4){         // checks if the input is more than length
+                cout<<"Enter name : ";          // if length id more than re takes input
+                cin>>name;                              // if meets the length then exits
         }
-        cout<<"Hey you did it by typing : "<<name;
+        cout<<"Hey you did it by typing : "<<name;      // displays the loop out
 
 return 0;
 }
